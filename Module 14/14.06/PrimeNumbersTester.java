763a9ea7a57eb2456fcfdf146401c6b275d2888e
JCampisi00 <40505503+JCampisi00@users.noreply.github.com>
2018-09-08 16:50:16 -0500

    Create PrimeNumbersTester.java

---
+++ Module 14/14.06/PrimeNumbersTester.java
@@ -0,0 +1,21 @@
+/**
+ * Purpose: tester class of PrimeNumbers
+ *
+ * @author Jared Campisi
+ * @version March 19th 2018
+ */
+public class PrimeNumbersTester
+{
+    //main method
+    public static void main (String [] args)
+    {
+        PrimeNumbers my = new PrimeNumbers();
+        
+        System.out.println("|====================================|");
+        System.out.println("|Welcome to my Prime Number Generator|");
+        System.out.println("|====================================|");
+        System.out.println("");
+               
+        my.userInput();
+    }
+}
